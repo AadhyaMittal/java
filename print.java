9584c85d8ac19ece912b27dc9e7f3128b616ae84
Aadhya Mittal <ammittalaadhya@gmail.com>
2016-09-08 22:46:44 +0530

    Create print.java

---
+++ print.java
@@ -0,0 +1,11 @@
+//To print a string using JAVA
+
+public class Solution
+{
+    public static void main(String[] args) 
+    {
+        /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
