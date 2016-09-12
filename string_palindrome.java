53d566a392c9f9d87d2f39904dd67771c688a6e1
Aadhya Mittal <ammittalaadhya@gmail.com>
2016-09-12 23:14:24 +0530

    Rename str_palindrome.java to string_palindrome.java

---
+++ string_palindrome.java
@@ -0,0 +1,34 @@
+//To tell if a string is PALINDROME or not
+
+import java.io.*;
+import java.util.*;
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        String A = scan.next();
+        int i, j;
+        int l = A.length();
+        boolean flag = true;
+        j = l - 1;
+        for(i = 0; i < l/2 ; i++)
+        {
+            if(A.charAt(i) != A.charAt(j))
+            {
+                flag = false;
+                break;
+            }
+            j--;
+        }    
+        if(flag == true)
+        {
+            System.out.println("Yes");
+        }
+        else
+        {
+            System.out.println("No");    
+        }
+    }
+}
